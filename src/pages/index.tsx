c0eef4c0030b59c2f478d4f98331bb028fff1e5c
t-tanakaa <s1f102401753@iniad.org>
2024-05-11 20:36:07 +0900

    重ねる

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,15 +3,42 @@ import styles from './index.module.css';
 
 const Home = () => {
   const [samplePos, setSanplePos] = useState(0);
-  console.log(samplePos);
+  const [board, setboard] = useState([
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+  ]);
+
+  const [bombmap, setbombmap] = useState([
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+  ]);
 
   return (
     <div className={styles.container}>
-      <div
-        className={styles.sampleStyle}
-        style={{ backgroundPosition: ` ${-30*samplePos}px 0px` }}
-      />
-      <button onClick={() => setSanplePos((p) => (p + 1) % 14)}>sample</button>
+      <div className={styles.flame}>
+        <div className={styles.board}>
+          {board.map((row, y) =>
+            row.map((color, x) => <div className={styles.cell} key={`${x}-${y}`} />),
+          )}
+          {board.map((row, y) =>
+            row.map((color, x) => <div className={styles.bombmap} key={`${x}-${y}`} />),
+          )}
+        </div>
+      </div>
     </div>
   );
 };
