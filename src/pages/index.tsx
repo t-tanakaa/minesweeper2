a73caaee3f5b718a4b892c9109a6fd8a6eeb7cc0
t-tanakaa <s1f102401753@iniad.org>
2024-05-11 22:53:56 +0900

    重ね成功次クリック反応

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -27,15 +27,22 @@ const Home = () => {
     [0, 0, 0, 0, 0, 0, 0, 0, 0],
   ]);
 
+//<div
+//className={styles.sampleStyle}
+//style={{ backgroundPosition: ` ${-30 * samplePos}px 0px` }}
+///>
+//<button onClick={() => setSanplePos((p) => (p + 1) % 14)}>sample</button>
+
   return (
     <div className={styles.container}>
       <div className={styles.flame}>
         <div className={styles.board}>
-          {board.map((row, y) =>
-            row.map((color, x) => <div className={styles.cell} key={`${x}-${y}`} />),
-          )}
-          {board.map((row, y) =>
-            row.map((color, x) => <div className={styles.bombmap} key={`${x}-${y}`} />),
+          {bombmap.map((row, y) =>
+            row.map((color, x) => (
+              <div className={styles.bombmap} key={`${x}-${y}`}>
+                <div className={styles.cell} key={`${x}-${y}`} />
+              </div>
+            )),
           )}
         </div>
       </div>
