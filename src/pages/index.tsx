6a26189515e112bdd9ff60975d17c7d92fc62fc8
t-tanakaa <s1f102401753@iniad.org>
2024-06-15 01:14:30 +0900

    爆弾連鎖はしたが赤く表示されてない

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -10,7 +10,6 @@ const directions = [
   [-1, -1],
   [-1, 0],
 ];
-
 const deployment = (bombMap: number[][], y: number, x: number, newBombMap: number[][]) => {
   const total = bombMap.flat().every((value) => value === 0);
   if (total === true) {
@@ -18,38 +17,34 @@ const deployment = (bombMap: number[][], y: number, x: number, newBombMap: numbe
     numberSelect(newBombMap);
   }
 };
-
 const bomb = (y: number, x: number, newBombMap: number[][]) => {
   let n = 0;
   while (10 > n) {
     const row = Math.floor(Math.random() * 9);
     const col = Math.floor(Math.random() * 9);
-    //console.log(row, col);
     if (newBombMap[row][col] !== -1 && y !== row && x !== col) {
       newBombMap[row][col] = -1;
       n++;
     }
   }
 };
-
-const numberSelect = (newBombMap: number[][]) => {
+const numberSelect = (bombMap: number[][]) => {
   for (let dy = 0; dy < 9; dy++) {
     for (let dx = 0; dx < 9; dx++) {
-      if (newBombMap[dy][dx] === -1) {
+      if (bombMap[dy][dx] === -1) {
         continue;
       }
       for (const direction of directions) {
         if (
-          newBombMap[dy + direction[0]] !== undefined &&
-          newBombMap[dy + direction[0]][dx + direction[1]] === -1
+          bombMap[dy + direction[0]] !== undefined &&
+          bombMap[dy + direction[0]][dx + direction[1]] === -1
         ) {
-          newBombMap[dy][dx] += 1;
+          bombMap[dy][dx] += 1;
         }
       }
     }
   }
 };
-
 const zeroChain = (
   y: number,
   x: number,
@@ -71,7 +66,6 @@ const zeroChain = (
   }
   console.log(lst2);
 };
-
 const zeroIndication = (lst2: [number, number][], newUserInputs: number[][]) => {
   for (const zeroNumber of lst2) {
     newUserInputs[zeroNumber[1]][zeroNumber[0]] = 1;
@@ -94,22 +88,20 @@ const Home = () => {
   const [bombMap, setBombMap] = useState(normalBoard);
   const board = normalBoard.map((row) => row.map(() => -1));
   const clickHandler = (x: number, y: number) => {
+    if (userInputs[y][x] === 2) {
+      return;
+    }
     const newUserInputs = structuredClone(userInputs);
     const newBombMap = structuredClone(bombMap);
     newUserInputs[y][x] = 1;
-    //console.table(newUserInputs);
     deployment(bombMap, y, x, newBombMap);
-    console.table(newBombMap);
     setBombMap(newBombMap);
     const lst2: [number, number][] = [];
     zeroChain(y, x, newBombMap, board, lst2);
     zeroIndication(lst2, newUserInputs);
     setUserInputs(newUserInputs);
-    //console.table(bombMap);
-    //sconsole.table(board);
   };
-
-  const rightClick = (event: React.MouseEvent<HTMLDivElement>, x: number, y: number) => {
+  const rightClickHandler = (event: React.MouseEvent<HTMLDivElement>, x: number, y: number) => {
     event.preventDefault();
     const newUserInputs = structuredClone(userInputs);
     if (userInputs[y][x] === 0) {
@@ -119,14 +111,20 @@ const Home = () => {
     }
     setUserInputs(newUserInputs);
   };
-
   const makeBoard = (userInput: number[][], bombMap: number[][], board: number[][]) => {
     for (let y = 0; y < 9; y++) {
       for (let x = 0; x < 9; x++) {
+        //ボム連鎖のコードが気持ち悪いから改善の余地あり
         if (userInput[y][x] === 1) {
           board[y][x] = 1;
           if (bombMap[y][x] === -1) {
-            board[y][x] = 11;
+            for (let y = 0; y < 9; y++) {
+              for (let x = 0; x < 9; x++) {
+                if (bombMap[y][x] === -1) {
+                  board[y][x] = 11;
+                }
+              }
+            }
           }
           for (let q = 1; q < 9; q++) {
             if (bombMap[y][x] === q) {
@@ -136,19 +134,15 @@ const Home = () => {
           if (bombMap[y][x] === 0) {
             board[y][x] = 0;
           }
-          if (userInput[y][x] === 2) {
-            board[y][x] = 12;
-          }
+        }
+        if (userInput[y][x] === 2) {
+          board[y][x] = 10;
         }
       }
     }
   };
   makeBoard(userInputs, bombMap, board);
-  //右クリックについて
-  //新しい関数を作る
-  //DIVと同期させる
-  //
-  //
+  console.table(bombMap);
   return (
     <div className={styles.container}>
       <div className={styles.flame}>
@@ -160,19 +154,18 @@ const Home = () => {
                 key={`${x}-${y}`}
                 style={{ backgroundPosition: ` ${-30 * (bombNumber - 1)}px 0px` }}
               >
-                {(bombNumber === -1 || bombNumber === 12) && (
+                {(bombNumber === -1 || bombNumber === 10) && (
                   <div
                     className={styles.stone}
                     onClick={() => clickHandler(x, y)}
-                    onContextMenu={(event) => rightClick(event, x, y)}
+                    onContextMenu={(event) => rightClickHandler(event, x, y)}
                   >
                     <div
                       className={styles.bombMap}
-                      style={{ backgroundPosition: ` ${-30 * (bombNumber - 10)}px 0px` }}
+                      style={{ backgroundPosition: ` ${-30 * (bombNumber - 1)}px 0px` }}
                     />
                   </div>
                 )}
-                {bombNumber !== -1 && bombNumber !== 12 && bombNumber !== -1 && <div>{bombNumber}</div>}
               </div>
             )),
           )}
@@ -181,5 +174,4 @@ const Home = () => {
     </div>
   );
 };
-
 export default Home;
